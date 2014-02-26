cba013414ccd49e02ef9f15688c3386b6455ce44
Hack Reactor Students <rob@robertwholmes.com>
2014-02-26 12:25:51 -0800

    Completed solution for functional instantiation style

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   instance.push = function(value){
+    // insert an item at the end
+    storage[size++] = value;
   };
 
   instance.pop = function(){
+    var item = storage[size-1];
+    if (item !== undefined) {
+      delete storage[size-1];
+      size--;
+    }
+    return item;
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
