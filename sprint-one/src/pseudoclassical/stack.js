725894d32a20661be46e61c175929aae239066a3
Hack Reactor Students <rob@robertwholmes.com>
2014-02-26 16:18:52 -0800

    Completed stack pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.stackSize = 0;
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.stackSize++] = value;
+};
+
+
+Stack.prototype.pop = function(){
+  var item = this.storage[this.stackSize-1];
+  if (item !== undefined) {
+    delete this.storage[this.stackSize-1];
+    this.stackSize--;
+  }
+  return item;
+};
+
+Stack.prototype.size = function(){
+  return this.stackSize;
 };
