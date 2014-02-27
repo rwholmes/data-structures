136570c3a91033683d103bbb5988971389536d4a
Hack Reactor Students <rob@robertwholmes.com>
2014-02-26 16:23:21 -0800

    Completed solution for pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,32 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.queueSize = 0;
+  this.topIndex = -1;
 };
+
+Queue.prototype.enqueue = function(value){
+  this.queueSize++;
+  this.storage[this.topIndex++] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  var index = (this.topIndex - 1) - (Object.keys(this.storage).length - 1);
+  var item = this.storage[index];
+
+
+  if (item !== undefined) {
+    delete this.storage[index];
+    this.queueSize--;
+  }
+
+  return item;
+};
+
+Queue.prototype.size = function(){
+  return this.queueSize;
+};
+
+
+
+
+
