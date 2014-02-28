3350ac7b119604d0209aa08e1545bf2c9b70fc93
Hack Reactor Students <rob@robertwholmes.com>
2014-02-27 17:00:23 -0800

    Completed tree! Added underscore library.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,11 @@
+/* global _ */
+/* exported makeTree */
+
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -9,8 +13,24 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  if (this.children === undefined) {
+    this.children = [];
+    this.children.push(makeTree(value));
+  } else {
+    this.children.push(makeTree(value));
+  }
 };
 
 treeMethods.contains = function(target){
-};
+  var result = false;
+  if (this.value === target) {
+    result = true;
+  }
+  if (this.children !== undefined) {
+    for (var i=0; i <this.children.length; i++) {
+      result = result || this.children[i].contains(target);
+    }
+  }
 
+  return result;
+};
