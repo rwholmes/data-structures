c761fc6053d8a35ca6103bd0566a7f93a2b467b0
Hack Reactor Students <rob@robertwholmes.com>
2014-03-01 16:34:31 -0800

    Refactored hastTable solution with methods used in lecture

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,52 +2,87 @@
 
 var HashTable = function(){
   this._limit = 8;
+  this._count = 0;
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
 
-  this._storage[i] = this._storage[i] || this._storage.set(i, []);
+  for (var j=0; j<bucket.length; j++){
+    var pair = bucket[j];
+    if (pair[0] === k) {
+      pair[1] = v;
+      return;
+    }
+  }
 
-  this._storage.get(i).push([k,v]);
+  bucket.push([k,v]);
+  this._storage.set(i, bucket);
 
-  this.adjustLimit();
+  this._count++;
+  if (this._count > 0.75 * this._limit){
+    this.adjustLimit(this._limit*2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
 
-  var bucket = this._storage.get(i);
+  for (var j=0; j<bucket.length; j++){
+    var pair = bucket[j];
+    if (pair[0] === k) {
+      return pair[1];
+    }
+  }
 
-  for (var j = 0; j <= bucket.length - 1; j++) {
-    if (bucket[j][0] === k) {
-      return bucket[j][1];
+};
+
+HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+
+  for (var j=0; j<bucket.length; j++){
+    var pair = bucket[j];
+    if (pair[0] === k) {
+      console.log('Bucket before slice: ' + bucket);
+      bucket.splice(j, 1);
+      console.log('Bucket after slice: ' + bucket);
+      this._count--;
+      if (this._count < 0.25 * this._limit && this._limit > 8){
+        this.adjustLimit(this._limit/2);
+      }
+      return pair[1];
     }
   }
 
   return null;
 };
 
-HashTable.prototype.remove = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(i);
+HashTable.prototype.adjustLimit = function(newLimit){
+  var oldStorage = this._storage;
 
-  for (var j = 0; j <= bucket.length - 1; j++) {
-    if (bucket[j][0] === k) {
-      bucket.splice(j, 1);
+  this._storage = makeLimitedArray(newLimit);
+  this._limit = newLimit;
+  this._count = 0;
+
+  var hashTable = this;
+
+  oldStorage.each(function(bucket){
+    if(!bucket){ return; }
+    for (var i=0; i<bucket.length; i++){
+      var pair = bucket[i];
+      hashTable.insert(pair[0], pair[1]);
     }
-  }
+  });
 };
 
-HashTable.prototype.adjustLimit = function(){
-  var size = Object.keys(this._storage).length;
 
-  if (size >= (3/4 * this._limit)) {
-    this._limit *= 2;
-    // need to makelimitedArray with new limit
-    // then need to move the values over to the new limited array
-  } else if (size <= (1/4 * this._limit)) {
-    this._limit *= 1/2;
-  }
-};
+
+
+
+
+
+
